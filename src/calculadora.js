c143f8c972b4b21f2b47f5dfb8401be1f214cee1
Natan <santonoliver1990@gmail.com>
2025-09-01 22:28:51 -0300

    adicionei a soma e o seu teste

---
+++ src/calculadora.js
@@ -0,0 +1,12 @@
+function somarDoisNumeros(valor1,valor2){
+    const resultado = valor1 + valor2;
+    return resultado;
+}
+
+function media(valor1,valor2,valor3){
+    const resultMedia = (valor1+valor2+valor3)/3;
+    return resultMedia;
+}
+module.exports = {
+   somarDoisNumeros 
+}
