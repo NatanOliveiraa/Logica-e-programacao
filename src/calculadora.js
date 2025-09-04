6f1d31f8f7da61d3e227daa62eddcf963091ed91
Natan <santonoliver1990@gmail.com>
2025-09-03 23:47:27 -0300

    Adicionei testes automatizados para a funcao

--- src/calculadora.js
+++ src/calculadora.js
@@ -2,11 +2,6 @@ function somarDoisNumeros(valor1,valor2){
     const resultado = valor1 + valor2;
     return resultado;
 }
-
-function media(valor1,valor2,valor3){
-    const resultMedia = (valor1+valor2+valor3)/3;
-    return resultMedia;
-}
 module.exports = {
-   somarDoisNumeros 
+   somarDoisNumeros
 }
--- test/calculadora.test.js
+++ test/calculadora.test.js
@@ -1,5 +1,45 @@
 const{somarDoisNumeros} = require('../src/calculadora');
+const{expect} = require('chai');
 
-const resultadoDaSoma = somarDoisNumeros(2,3);
 
-console.log(resultadoDaSoma);
+// Describe é uma função do mocha e ela tem 2 (duas) entradas (primeira entrada um texto - grupo) a segunda
+//entrada é uma função anônima function () ou ()=>
+describe('Testes da função soma',()=>{   
+    it('A função deve ser capaz de somar dois números positivos',()=>{
+         //Coleto o resultado da função
+        const resultadoDaSoma = somarDoisNumeros(5,10);
+
+        /*Comparo o resultado com o valor esperado
+          Nesse caso faço uso do chai para fazer a comparação
+          através do comando const{expect} = require('chai')
+          após isso, posso utilizar a função para comparar
+          expect( resultado da soma).to.equal(resultado esperado)
+
+        */ 
+        expect(resultadoDaSoma).to.equal(15);
+    });
+    it('A função deve ser capaz de somar um número positivo e um negativo',()=>{
+        //coleto o resultado da função
+        const resultadoDaSoma = somarDoisNumeros(50,-30);
+
+        //comparo o resultado obtido com o resultado esperado
+        expect(resultadoDaSoma).to.equal(20);
+
+    });
+    it('A função deve ser capaz de somar dois números negativos',()=>{
+        //coleto o resultado da função
+        const resultadoDaSoma = somarDoisNumeros(-20,-30);
+
+        //comparo o resultado obtido com o resultado esperado
+        expect(resultadoDaSoma).to.equal(-50);
+    });
+     it('A função deve ser capaz de somar dois números zerados',()=>{
+        //Coleto o resultado da função
+        const resultadoDaSoma = somarDoisNumeros(0,0);
+
+        //Comparo o valor obtido com o valor esperado
+        expect(resultadoDaSoma).to.equal(0);
+    });
+
+});
+
